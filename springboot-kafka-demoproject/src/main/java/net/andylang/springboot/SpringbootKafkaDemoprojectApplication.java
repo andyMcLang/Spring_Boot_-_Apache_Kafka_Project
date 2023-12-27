f00ace6cf038c7dad8a7950e8419ec1ae944555e
Andy Lang <djdreviz@gmail.com>
2023-12-27 22:44:20 +0200

    Create and Setup Spring Boot Project in IntelliJ done

---
+++ springboot-kafka-demoproject/src/main/java/net/andylang/springboot/SpringbootKafkaDemoprojectApplication.java
@@ -0,0 +1,13 @@
+package net.andylang.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaDemoprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKafkaDemoprojectApplication.class, args);
+	}
+
+}
